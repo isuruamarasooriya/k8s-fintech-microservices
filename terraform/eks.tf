6d8c2885793b8248651489da0ddf97d0875f796f
Isuru Amarasooriya <isuruamarasooriya177@gmail.com>
2026-08-02 20:44:22 +0530

    Add new Update

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -11,10 +11,14 @@ module "eks" {
 
   eks_managed_node_groups = {
     main_nodes = {
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
       instance_types = ["t3.small"] 
+
+      iam_role_additional_policies = {
+        dynamodb_policy = aws_iam_policy.node_dynamodb_policy.arn
+      }
     }
   }
 
@@ -24,4 +28,27 @@ module "eks" {
     Project     = var.project_name
     Environment = "dev"
   }
+}
+
+resource "aws_iam_policy" "node_dynamodb_policy" {
+  name        = "${var.project_name}-node-dynamodb-policy-v2"
+  description = "Allow EKS nodes to write and read/scan DynamoDB transactions table"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:Scan",
+          "dynamodb:Query",
+          "dynamodb:GetItem",
+          "dynamodb:DescribeTable"
+        ]
+        Resource = aws_dynamodb_table.transactions.arn
+      }
+    ]
+  })
 }
